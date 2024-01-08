de299e46b37aaa5da0d460ccf615e0e0c49d7800
Lightness322 <dyubkov99@mail.ru>
2024-01-08 17:56:01 +0500

    add env

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl = "https://syqduwcsavdxsqlxwrwi.supabase.co"
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5cWR1d2NzYXZkeHNxbHh3cndpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM0MTYxNTUsImV4cCI6MjAxODk5MjE1NX0.Zz9tk4zvTDbRiUCzUIrvM8niQmRPEV-sc9yQvDdrHi4"
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
