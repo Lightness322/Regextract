4b3d0bea1090b5e97f68f8e046499dcae5f0660e
Lightness322 <dyubkov99@mail.ru>
2024-01-08 18:19:59 +0500

    test netlify env

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,16 @@
+import { Netlify } from "@netlify/edge-functions/node/dist/bootstrap/globals"
 import { createClient } from "@supabase/supabase-js"
 
+const supabaseKey = await getApiKey()
+
 const supabaseUrl = "https://syqduwcsavdxsqlxwrwi.supabase.co"
-const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+// const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY
+const supabase = createClient(supabaseUrl, supabaseKey!)
 
 export default supabase
+
+export async function getApiKey() {
+  const supabaseKey = await Netlify.env.get("VITE_SUPABASE_API_KEY")
+
+  return supabaseKey
+}
